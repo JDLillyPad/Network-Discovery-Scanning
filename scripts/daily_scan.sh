8dc071074efa4b2854567dbbcdb8170e516f255f
JDLillyPad <239805697+JDLillyPad@users.noreply.github.com>
2026-02-10 17:24:50 -0500

    Add description comment to daily_scan.sh
    Added a comment to describe the purpose of the script.

--- scripts/daily_scan.sh
+++ scripts/daily_scan.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# Runs a daily Nmap ping scan and stores results for baseline comparison
 DATE=$(date +"%Y-%m-%d")
 # I've entered a loopback address but feel free to input your CIDR IP Address
 TARGET="127.0.0.1/24"
