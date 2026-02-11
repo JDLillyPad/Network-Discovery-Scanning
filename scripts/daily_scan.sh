89788b80ab466ce685093c6b70186b95dede8f3a
JDLillyPad <239805697+JDLillyPad@users.noreply.github.com>
2026-02-11 07:48:37 -0500

    Change output directory for Nmap scan results

--- scripts/daily_scan.sh
+++ scripts/daily_scan.sh
@@ -3,7 +3,7 @@
 DATE=$(date +"%Y-%m-%d")
 # I've entered a loopback address but feel free to input your CIDR IP Address
 TARGET="127.0.0.1/24"
-OUTPUT="$HOME/scans/nmap_$DATE.txt"
+OUTPUT="$HOME/filepathtoscans/nmap_$DATE.txt"
 
 nmap -sn $TARGET > $OUTPUT
 
